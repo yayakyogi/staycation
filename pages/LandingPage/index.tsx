378b44427d1cd6f891ce10bfe998b93492e31904
Yayak Yogi <yayaktaka@gmail.com>
2021-10-13 14:22:09 +0700

    add comonent categories

--- pages/LandingPage/index.tsx
+++ pages/LandingPage/index.tsx
@@ -1,13 +1,15 @@
 import Headers from "../../parts/Header";
 import Hero from "../../parts/Hero";
 import MostPicked from "../../parts/MostPicked";
+import Categories from "../../parts/Categories";
 
 import landingPage from "../../dummy/landingPage";
 
 export default function LandingPage() {
   return (
     <>
-      <Headers /> <Hero /> <MostPicked data={landingPage.mostPicked} />
+      <Headers /> <Hero /> <MostPicked data={landingPage.mostPicked} />{" "}
+      <Categories data={landingPage.categories} />
     </>
   );
 }
