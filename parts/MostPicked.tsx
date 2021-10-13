d69756b645b5e1d95822b21a37d6668135fcc82f
Yayak Yogi <yayaktaka@gmail.com>
2021-10-13 13:02:41 +0700

    fix responsive component most picked

--- parts/MostPicked.tsx
+++ parts/MostPicked.tsx
@@ -1,27 +1,31 @@
 import React from "react";
 import Image from "next/image";
 
-export default function MostPicked(props) {
-  // console.log("Data: ", props.data[0].name);
+import Button from "../components/button";
 
+export default function MostPicked(props) {
   return (
     <div
       id="mostpicked"
-      className="container mx-auto mt-16 px-8 md:px-16 pb-20"
+      className="container mx-auto mt-16 px-8 md:px-16 pb-20 pt-8 md:pt-0"
     >
       <h1 className="poppins-medium text-2xl text-gray900 mb-9">Most Picked</h1>
-      <div className="grid grid-rows-4 grid-flow-col gap-4">
+      <div className="grid grid-cols-4 md:grid-rows-4 grid-flow-row md:grid-flow-col gap-4">
         {props.data.map((items, index) => {
           return (
             <div
               key={`mostpicked-${index}`}
-              className={`${index === 0 ? "row-span-4" : "row-span-2"}`}
+              className={`${
+                index === 0
+                  ? "col-span-4 md:row-span-4"
+                  : "col-span-4 md:row-span-2"
+              }`}
             >
               <div className="rounded-2xl relative bg-gradient-to-t card">
-                <div className="absolute z-10 right-0 bg-pink rounded-tr-2xl rounded-bl-2xl px-10 py-2">
-                  <span className="text-base poppins-medium text-white">
+                <div className="absolute z-10 right-0 bg-pink rounded-tr-2xl rounded-bl-2xl px-6 md:px-10 py-1 md:py-2">
+                  <span className="text-sm md:text-base poppins-medium text-white">
                     ${items.price}{" "}
-                    <span className="text-base poppins-light text-white">
+                    <span className="text-sm md:text-base poppins-light text-white">
                       per night
                     </span>
                   </span>
@@ -35,9 +39,13 @@ export default function MostPicked(props) {
                   />
                 </figure>
                 <div className="absolute bottom-4 left-4 z-20">
-                  <span className="text-xl poppins-regular text-white">
+                  <Button
+                    type="link"
+                    href={`/properties/${items._id}`}
+                    className="text-xl poppins-regular text-white"
+                  >
                     {items.name}
-                  </span>
+                  </Button>
                   <br />
                   <span className="text-sm poppins-light text-white">
                     {items.city},{items.country}
