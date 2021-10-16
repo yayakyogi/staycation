3dba3bb012e459b87ed70be70d9b9ca734537142
Yayak Yogi <yayaktaka@gmail.com>
2021-10-17 00:22:36 +0700

    refactoring code

--- parts/Header.tsx
+++ parts/Header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React, { useState } from "react";
 import { useRouter } from "next/router";
+
 import BrandIconText from "../components/brandIconText";
 import Button from "../components/button";
 
